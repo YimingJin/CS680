43f2f62f107e04f8047de37f7c11a497a7f81ae4
Jim-Jin001 <544209275@qq.com>
2020-11-17 23:38:53 -0500

    HW09 version1

---
+++ HW9/src/edu/umb/cs680/hw09/apfs/APFS.java
@@ -0,0 +1,24 @@
+package edu.umb.cs680.hw09.apfs;
+
+import edu.umb.cs680.hw09.fs.FSElement;
+import edu.umb.cs680.hw09.fs.FileSystem;
+
+import java.time.LocalDateTime;
+
+public class APFS extends FileSystem {
+    private LocalDateTime localTime = LocalDateTime.now();
+    private static APFS instance = null;
+    private ApfsDirectory root;
+    public static APFS getAPFSFileSystem() {
+        if (instance == null)
+            instance = new APFS();
+        return instance;
+    }
+    protected FSElement createDefaultRoot() {
+        root = new ApfsDirectory(null, "root", 0, localTime, "ApfsFile", localTime);
+        return root;
+    }
+    public ApfsDirectory getRootDir() {
+        return root;
+    }
+}
---
+++ HW9/test/src/edu/umb/cs680/hw09/APFSTest.java
@@ -0,0 +1,41 @@
+package edu.umb.cs680.hw09;
+
+import edu.umb.cs680.hw09.apfs.APFS;
+import edu.umb.cs680.hw09.apfs.ApfsDirectory;
+import edu.umb.cs680.hw09.apfs.ApfsElement;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class APFSTest {
+    private String[] apfsElementToArray(ApfsElement e) {
+        String[] elementInfo = {
+                Boolean.toString(e.isDirectory()),
+                e.getName(),
+                Integer.toString(e.getSize()),
+                e.getOwnerName()
+        };
+        return elementInfo;
+    }
+    @Test
+    public void sameAPFSTest() {
+        APFS APFSOne = APFS.getAPFSFileSystem();
+        APFS APFSTwo = APFS.getAPFSFileSystem();
+        assertSame(APFSOne, APFSTwo);
+    }
+    @Test
+    public void getRootDirTest() {
+        APFS ApfsFileSystem = APFS.getAPFSFileSystem();
+        ApfsFileSystem.initFileSystem("ApfsFile", 500);
+        ApfsDirectory actual = ApfsFileSystem.getRootDir();
+        String[] expected = { "true","root", "0","ApfsFile" };
+        assertArrayEquals(expected,apfsElementToArray(actual));
+    }
+    @Test
+    public void createDefaultRootTest() {
+        APFS ApfsFileSystem = APFS.getAPFSFileSystem();
+        ApfsDirectory actual = ApfsFileSystem.getRootDir();
+        String[] expected = { "true","root", "0", "ApfsFile" };
+        assertArrayEquals(expected, apfsElementToArray(actual));
+    }
+}
